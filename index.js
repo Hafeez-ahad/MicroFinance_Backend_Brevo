95f186581fbf36d62d23169559f973974235d8d4
Hafeez-ahad <itxhafeex252@gmail.com>
2025-04-25 12:31:21 +0500

    12:31

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 
 app.use(cors({
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
